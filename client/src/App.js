8da375e868007417492f57446c18f4d427355d24
Bruno A <bruno_arruda_ba@hotmail.com>
2023-03-25 01:19:55 -0300

    :sparkles: feat: Redux and toolkit installation and setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<LoginPage />}></Route>
+               <Route path="/home" element={<HomePage />}></Route>
+               <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import authReducer from "./state";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, FLUSH } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+   reducer: persistReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: {
+            ignoreActions: [REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, FLUSH],
+         },
+      }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
-      <App />
+      <Provider store={store}>
+         <PersistGate loading={null} persistor={persistStore}>
+            <App />
+         </PersistGate>
+      </Provider>
    </React.StrictMode>
 );
