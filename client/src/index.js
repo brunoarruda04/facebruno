e354b9c2146e7d2d0f56a5005bb4a8192c1eb69d
Bruno A <bruno_arruda_ba@hotmail.com>
2023-03-25 01:45:49 -0300

    :bug: fix: Fixing redux config

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const store = configureStore({
    middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
          serializableCheck: {
-            ignoreActions: [REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, FLUSH],
+            ignoredActions: [REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, FLUSH],
          },
       }),
 });
@@ -24,7 +24,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
       <Provider store={store}>
-         <PersistGate loading={null} persistor={persistStore}>
+         <PersistGate loading={null} persistor={persistStore(store)}>
             <App />
          </PersistGate>
       </Provider>
