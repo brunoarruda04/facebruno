f323562a36c87e0c3044898ba15e59e64a7b6972
Bruno A <bruno_arruda_ba@hotmail.com>
2023-03-25 10:28:18 -0300

    :sparkles: feat: Navbar and themes by state

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery } from "@mui/material";
+import { Search, Mesasge, DarkMode, LightMode, Notifications, Menu, Help, Close } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
-   return <div>Navbar</div>;
+   const [isMobileMenuToggled, setisMobileMenuToggled] = useState(false);
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const user = useSelector((state) => state.user);
+   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+   const theme = useTheme();
+   const neutralLight = theme.pallete.neutral.light;
+   const dark = theme.pallete.neutral.dark;
+   const background = theme.pallete.background.default;
+   const primaryLight = theme.palette.primary.light;
+   const alt = theme.palette.background.alt;
+
+   const fullName = `${user.firstName} ${user.lastName}`;
+
+   return <FlexBetween padding="1rem 6%" backgroundColor={alt}></FlexBetween>;
 };
 
 export default Navbar;
